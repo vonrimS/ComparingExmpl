28fa83e4e93f12c0e628ae85abf829ec61aab921
mName <mr.mytab@gmail.com>
2017-05-02 01:04:52 +0300

    some CompareStuff classes were changed for lambda from functional interface "CompareThem"

--- CompareTheseExmpl/src/com/mname/compare/these/Main.java
+++ CompareTheseExmpl/src/com/mname/compare/these/Main.java
@@ -1,5 +1,6 @@
 package com.mname.compare.these;
 
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -7,17 +8,32 @@ import java.util.TreeMap;
 public class Main {
 
 	public static void main(String[] args) {
-		new CompareThese();
 
-		TreeMap<String, Double> tm = new TreeMap<String, Double>((String o1, String o2) -> o1.compareTo(o2));
+		CompareThem compareThem = (String a, String b) -> {
+			int i = a.lastIndexOf(' ');
+			int j = b.lastIndexOf(' ');
+			return a.substring(i).compareToIgnoreCase(b.substring(j));
+		};
+
+		CompareThem compareThemFirst = (String a, String b) -> {
+			return a.compareToIgnoreCase(b);
+		};
+
+		Comparator<String> compareThen = compareThem.thenComparing(compareThemFirst);
+
+		TreeMap<String, Double> tm = new TreeMap<String, Double>(compareThen);
 
 		tm.put("Алексей Иванов", new Double(1000.10));
 		tm.put("Григорий Иванов", new Double(1890.19));
-		tm.put("Инвар Гайдар", new Double(99000.10));
+		tm.put("Инвар Aйдар", new Double(900.10));
 		tm.put("Савелий Брюлов", new Double(101.11));
 		tm.put("Карл Фарел", new Double(1000.10));
+		tm.put("Гри Иванов", new Double(8890.19));
 		tm.put("Гарик Мартиросян", new Double(1660.10));
 		tm.put("Джон Кеннеди", new Double(19990.99));
+		tm.put("Гиви Гиви", new Double(20002.99));
+		tm.put("Савелий Сеюлов", new Double(101.11));
+		tm.put("Карл Каррел", new Double(1000.10));
 
 		Set<Map.Entry<String, Double>> set = tm.entrySet();
 
